34310e653989ba76f7e475ece6483e7f9443c69a
thejsj <jorge.silva@thejsj.com>
2015-07-20 11:15:29 -0700

    Adding connection parsing

--- server/buffer-parser.js
+++ server/buffer-parser.js
@@ -8,10 +8,59 @@ const BufferParser = class BufferParser {
   constructor () {
     this[_queue_] = new Buffer(0);
     this[_listeners_] = {};
+    this.protoVersion = null;
+    this.authKeyLength = null;
+    this.authKey = null;
+    this.protoProtocol = null;
   }
 
   append (buff) {
-    this[_queue_] = Buffer.concat([this[_queue_], buff]);
+    if (buff !== undefined) {
+      this[_queue_] = Buffer.concat([buff, buff]);
+    }
+    if (this.protoVersion === null) {
+      if (Buffer.byteLength(this[_queue_]) >= 4) {
+        this.protoVersion = this[_queue_].slice(0, 4).readUInt32LE();
+        this[_queue_] = this[_queue_].slice(4);
+        // Call code again to ensure we don't have the authKey
+        this.append();
+      }
+    } else if (this.authKeyLength === null) {
+      if (Buffer.byteLength(this[_queue_] >= 4)) {
+        this.authKeyLength = this[_queue_].slice(0, 4).readUInt32LE();
+        this[_queue_] = this[_queue_].slice(4);
+        // Call code again to ensure we don't have the authKey
+        this.append();
+      }
+    } else if (this.authKey === null) {
+      if (Buffer.byteLength(this[_queue_] >= this.authKeyLength)) {
+        this.authKey = this[_queue_].slice(0, this.authKeyLength).toString('ascii');
+        this[_queue_] = this[_queue_].slice(this.authKeyLength);
+        // Call code again to ensure we don't have the authKey
+        this.append();
+      }
+    } else if (this.protoProtocol === null) {
+      if (Buffer.byteLength(this[_queue_] >= 4)) {
+        this.protoProtocol = this[_queue_].slice(0, 4).readUInt32LE();
+        this[_queue_] = this[_queue_].slice(4);
+        // Call code again to ensure we don't have the authKey
+        this.append();
+        this.fireEvent('connect', this.protoVersion, this.authKey, this.protoProtocol);
+      }
+    } else {
+      this.parseQuery();
+    }
+  }
+
+  fireEvent (eventName, ...data) {
+    if (this[_listeners_][eventName] !== undefined) {
+      this[_listeners_][eventName].forEach((func) => {
+        func.apply(null, data);
+      });
+    }
+  }
+
+  parseQuery () {
     let splitString = this[_queue_].toString().split('');
     let openBrackets = 0;
     let foundBracket = false;
@@ -29,18 +78,13 @@ const BufferParser = class BufferParser {
         let query = this[_queue_].slice(12, i + 1);
         if (Buffer.byteLength(query) === byteLength) {
           let json = JSON.parse(query.toString());
-          if (this[_listeners_].query !== undefined) {
-            this[_listeners_].query.forEach((func) => {
-              func(json, token);
-            });
-          }
+          this.fireEvent('query', json, token);
         }
         if (i + 1 >= splitString.length) this[_queue_] = new Buffer(splitString.slice(i + 1));
         else this[_queue_] = new Buffer(0);
         break;
       }
     }
-
   }
 
   on (eventName, func) {
