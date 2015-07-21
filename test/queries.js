f157f8a9da9a1ac8a3847f718b670af70f086332
thejsj <jorge.silva@thejsj.com>
2015-07-21 11:56:05 -0700

    Adding server closing

--- test/queries.js
+++ test/queries.js
@@ -3,36 +3,64 @@ import r from 'rethinkdb';
 import Promise from 'bluebird';
 import server from '../server';
 import should from 'should';
+import { makeExecuteQuery, makeAssertQuery, makeCreateDatabase, makeDropDatabase } from './utils';
 
 let proxyPort = 8125;
+let dbName = 'rethinkdb_proxy_test';
+let tableName = 'entries';
 
-let assertQuery = ((query) => {
-  return Promise.resolve()
-      .then(() => {
-        return [r.connect(), r.connect({ port: proxyPort })];
-      })
-      .spread((connA, connB) => {
-        return [query.run(connA), query.run(connB)];
-      })
-      .spread((resultA, resultB) => {
-        resultA.should.eql(resultB);
-        return;
-      });
-});
+let executeQuery = makeExecuteQuery(dbName, proxyPort);
+let assertQuery = makeAssertQuery(executeQuery);
+let createDatabase = makeCreateDatabase(dbName, tableName);
+let dropDatabase = makeDropDatabase(dbName);
 
-describe('Read Queries', () => {
+describe('Queries', () => {
 
   before((done) => {
-    server(proxyPort, done);
+    createDatabase()
+      .then(() => {
+        server(proxyPort, done);
+      });
   });
 
-  it('should return an `r.expr` successfully', (done) => {
-    assertQuery(r.expr([1, 2, 3]))
-     .then(done);
+  after((done) => {
+    dropDatabase()
+      .nodeify(done);
   });
 
-  it('should return the same list of databases', (done) => {
-    assertQuery(r.dbList())
-     .then(done);
+  describe('Read Queries', () => {
+
+     it('should return an `r.expr` successfully', (done) => {
+      assertQuery(r.expr([1, 2, 3]))
+       .nodeify(done);
+    });
+
+    it('should return the same list of databases', (done) => {
+      assertQuery(r.dbList())
+       .nodeify(done);
+    });
+
   });
+
+  describe('Write Queries', () => {
+
+    it('should return the same result after a write', (done) => {
+      let get = r.db(dbName).table(tableName);
+      executeQuery(get.insert({ hello: 'world'}))
+       .then(() => {
+         return assertQuery(get);
+       })
+       .then(() => {
+         return r.connect().then((conn) => {
+           return get.count().run(conn)
+             .then((count) => {
+               count.should.eql(2);
+             });
+         });
+       })
+       .nodeify(done);
+    });
+
+  });
+
 });
