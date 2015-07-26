2aa344b884f8020851e81a44327bfb2366aa6909
thejsj <jorge.silva@thejsj.com>
2015-07-26 12:33:30 -0700

    Add CLI

--- TODO.md
+++ TODO.md
@@ -1,26 +1,29 @@
 # TODO
 
-[x] Add token/connection parser
-[x] Prevent `insert`, `update`, `replace`, `delete`
 [ ] Allow only a single database/table
   [ ] database `--db test,hello`
   [ ] tables `--table people, trains`, `--table test.people, hello.trains`
-[ ] Opt-in Options:
-  [ ] Allow Insert `--allow-insert`
-  [ ] Allow Delete`--allow-delete`
-  [ ] Allow Update`--allow-update`
-  [ ] Allow Database Create `--allow-db-create`
-  [ ] Database Drop `--allow-db-drop`
-  [ ] Table Creation `--allow-table-create`
-  [ ] Table Drop `--allow-table-drop`
-  [ ] HTTP `--allow-http`
-  [ ] JavaScript `--allow-javascript`
-  [ ] Reconfigure `--allow-reconfigure`
-  [ ] Rebalance `--allow-rebalance`
 
 ### Future
 
 [ ] Use from browser
-  [ ] Replace `net` library
-[ ] Convert queries back to strings. Re-build AST.
+  [ ] Replace `net` library with websockets
 [ ] Authentication
+[ ] Convert queries back to strings. Re-build AST.
+
+### Done
+
+[x] Opt-in Options:
+  [x] Allow Insert `--allow-insert`
+  [x] Allow Delete`--allow-delete`
+  [x] Allow Update`--allow-update`
+  [x] Allow Database Create `--allow-db-create`
+  [x] Database Drop `--allow-db-drop`
+  [x] Table Creation `--allow-table-create`
+  [x] Table Drop `--allow-table-drop`
+  [x] HTTP `--allow-http`
+  [x] JavaScript `--allow-javascript`
+  [x] Reconfigure `--allow-reconfigure`
+  [x] Rebalance `--allow-rebalance`
+[x] Add token/connection parser
+[x] Prevent `insert`, `update`, `replace`, `delete`
---
+++ bin/rethinkdb-proxy.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+var cli = require('cli').enable('status');
+var taser = require('taser');
+require("babel/register");
+var rethinkDBProxy = require('../server/');
+
+cli.parse({
+  'port':               [false, 'Port in which to run rethinkdb-proxy', 'number', null],
+  'allow-writes':       [false, 'Allow all operations that write to the database (`insert`, `update`, `delete`)'],
+  'allow-insert':       [false, 'Allow `insert` queries'],
+  'allow-update':       [false, 'Allow `update` queries'],
+  'allow-delete':       [false, 'Allow `delete` queries'],
+  'allow-replace':      [false, 'Allow `replace` queries'],
+  'allow-db-create':    [false, 'Allow `dbCreate` queries'],
+  'allow-db-drop':      [false, 'Allow `dbDrop` queries'],
+  'allow-table-create': [false, 'Allow `tableCreate` queries'],
+  'allow-table-drop':   [false, 'Allow `tableDrop` queries'],
+  'allow-indexes':      [false, 'Allow all operations on indexes (`indexCreate`, `indexDrop`, `indexRename`)'],
+  'allow-index-create': [false, 'Allow `indexCreate` queries'],
+  'allow-index-drop':   [false, 'Allow `indexDrop` queries'],
+  'allow-index-rename': [false, 'Allow `indexRename` queries'],
+  'allow-reconfigure':  [false, 'Allow `reconfigure` queries'],
+  'allow-rebalance':    [false, 'Allow `rebalance` queries'],
+  'allow-http':         [false, 'Allow queries with the `http` term'],
+  'allow-javascript':   [false, 'Allow queries with the `js` term']
+});
+
+cli.main(function (args, _opts) {
+  var opts = {};
+  for (var key in _opts) {
+    if (_opts.hasOwnProperty(key) && _opts[key] !== null) {
+      opts[key.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })] = _opts[key];
+    }
+  }
+  rethinkDBProxy(opts);
+});
