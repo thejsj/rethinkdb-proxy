8475c52a1cb04070b67e67ebad098c4a8aaa5f2c
thejsj <jorge.silva@thejsj.com>
2015-08-07 09:12:26 -0700

    Add parallel tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
 /*jshint esnext:true */
 
+import './parser.js';
 import './connection.js';
 import './queries.js';
 import './read-only-queries.js';
 import './edge-cases.js';
 import './database-and-table-access.js';
 import './changefeeds.js';
+import './parallel.js';
 
 
