6524c9680299ca4e99412ae849555b7cca3c0262
thejsj <jorge.silva@thejsj.com>
2015-08-09 10:21:16 -0700

    Changing JSON parsing

--- server/buffer-parser.js
+++ server/buffer-parser.js
@@ -25,7 +25,7 @@ const BufferParser = class BufferParser {
         this.append();
       }
     } else if (this.authKeyLength === null) {
-      if (Buffer.byteLength(this[_queue_] >= 4)) {
+      if (Buffer.byteLength(this[_queue_]) >= 4) {
         this.authKeyLength = this[_queue_].slice(0, 4).readUInt32LE();
         this[_queue_] = this[_queue_].slice(4);
         // Call code again to ensure we don't have the authKey
@@ -39,7 +39,7 @@ const BufferParser = class BufferParser {
         this.append();
       }
     } else if (this.protoProtocol === null) {
-      if (Buffer.byteLength(this[_queue_] >= 4)) {
+      if (Buffer.byteLength(this[_queue_]) >= 4) {
         this.protoProtocol = this[_queue_].slice(0, 4).readUInt32LE();
         this[_queue_] = this[_queue_].slice(4);
         // Call code again to ensure we don't have the authKey
@@ -60,46 +60,41 @@ const BufferParser = class BufferParser {
   }
 
   parseQuery () {
-    let splitString = this[_queue_].toString().split('');
-    let openBrackets = 0;
-    let foundBracket = false;
-    let double_string_open = false;
-    let single_string_open = false;
+    /*!
+     * Assumptions
+     * 1. All clients have their own instance of BufferParser
+     * 2. TCP connections send messages in order
+     * 3. Because messages are sent in order by the client through a TCP connection,
+     * we can assume that all packets are tokens followed by query bytelenghts
+     * followed by queries
+     */
     let newQueue = null;
-    let ch, prevCh;
-    if (splitString.length <= 12) return;
-    for (let i = 12; i < splitString.length; i += 1) {
-      let ch = splitString[i];
-      if (!double_string_open && !single_string_open && ch === '[') {
-        openBrackets += 1;
-        foundBracket = true;
-      } else if (!double_string_open && !single_string_open && ch === ']') {
-        openBrackets -= 1;
-      } else if (ch === '"') {
-        if (!single_string_open && prevCh !== '/') {
-          double_string_open = !double_string_open;
-        }
-      } else if (ch === "'") {
-        if (single_string_open && prevCh !== '/' && !double_string_open) {
-          single_string_open = !single_string_open;
-        }
-      }
-      if (openBrackets === 0 && foundBracket) {
-        let token = this[_queue_].slice(0, 8).readUInt32LE();
-        let byteLength = this[_queue_].slice(8, 12).readUInt32LE();
-        let query = this[_queue_].slice(12, i + 1);
-        newQueue = this[_queue_].slice(i + 1);
-        if (Buffer.byteLength(query) === byteLength) {
-          let json = JSON.parse(query.toString());
-          this[_queue_] = new Buffer(0);
-          this.fireEvent('query', json, token);
-        }
-        break;
-      }
-      prevCh = ch;
+    let _prevQuery = null;
+    /*!
+     * Make sure we have all three components necessary to parse the query:
+     * token, byteLength, and query
+     */
+    if (Buffer.byteLength(this[_queue_]) <= 12) return;
+    let token = this[_queue_].slice(0, 8).readUInt32LE();
+    let byteLength = this[_queue_].slice(8, 12).readUInt32LE();
+    let query = this[_queue_].slice(12, byteLength + 12);
+    newQueue = this[_queue_].slice(byteLength + 12 + 1);
+    try {
+      // Simplest way to handle check if input is valid JSON
+      let json = JSON.parse(query.toString());
+      this.fireEvent('query', json, token);
+    } catch (err) {
+      // I think the problem has something to do with the fact that I'm adding a
+      // comma somehwere ....
+      console.log('JSON PARSE ERROR', err);
+      this.fireEvent('error', token);
     }
-    if (newQueue !== null && newQueue.length > 0) {
-      this.append(newQueue);
+    this[_queue_] = new Buffer(0);
+    if (newQueue !== null && newQueue.length > 0 && Buffer.byteLength(newQueue) >= 8){
+      let token = newQueue.slice(0, 8).readUInt32LE();
+      if (parseInt(token) === +token) {
+        this.append(newQueue);
+      }
     }
   }
 
